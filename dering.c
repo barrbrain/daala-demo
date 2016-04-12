bdbed1882cd543e2ab089a1bae3facc63678cdec
David Michael Barr <b@rr-dav.id.au>
2016-04-12 22:41:46 +0900

    Mangle the deringing code into small JS
    Signed-off-by: David Michael Barr <b@rr-dav.id.au>

--- dering.c
+++ dering.c
@@ -22,34 +22,65 @@ CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
 OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.*/
 
-#ifdef HAVE_CONFIG_H
-# include "config.h"
-#endif
-
 #include <stdlib.h>
-#include <math.h>
-#include "dering.h"
+#include <emscripten.h>
+typedef int od_coeff;
+# define OD_COEFF_BITS (32)
+# define OD_COEFF_SHIFT (4)
 
-const od_dering_opt_vtbl OD_DERING_VTBL_C = {
-  {od_filter_dering_direction_4x4_c, od_filter_dering_direction_8x8_c},
-  {od_filter_dering_orthogonal_4x4_c, od_filter_dering_orthogonal_8x8_c}
+# define OD_MINI(a, b) ((a) ^ (((b) ^ (a)) & -((b) < (a))))
+
+# define OD_CLZ0 (32)
+# define OD_CLZ(x) (__builtin_clz(x))
+# define OD_ILOG_NZ(x) (OD_CLZ0 - OD_CLZ(x))
+# define OD_ILOG(x) (OD_ILOG_NZ(x) & -!!(x))
+
+/*Smallest blocks are 8x8*/
+# define OD_LOG_BSIZE0 (3)
+/*There are 1 block sizes total (8x8).*/
+# define OD_NBSIZES    (1)
+/*The log of the maximum length of the side of a block.*/
+# define OD_LOG_BSIZE_MAX (OD_LOG_BSIZE0 + OD_NBSIZES - 1)
+/*The maximum length of the side of a block.*/
+# define OD_BSIZE_MAX     (1 << OD_LOG_BSIZE_MAX)
+/*The maximum number of quad tree levels when splitting a super block.*/
+# define OD_MAX_SB_SPLITS (OD_NBSIZES - 1)
+
+#define OD_DERING_LEVELS (6)
+typedef int od_dering_in;
+
+#define OD_DERING_NBLOCKS (OD_BSIZE_MAX/8)
+
+#define OD_FILT_BORDER (3)
+#define OD_FILT_BSTRIDE (OD_BSIZE_MAX + 2*OD_FILT_BORDER)
+
+EMSCRIPTEN_KEEPALIVE
+__attribute__((noinline))
+static void init_tables(volatile int *x) {
+  int i;
+  int stride = OD_FILT_BSTRIDE;
+  *x++ = -1*stride + 1, *x++ = -2*stride + 2, *x++ = -3*stride + 3,
+  *x++ =  0*stride + 1, *x++ = -1*stride + 2, *x++ = -1*stride + 3,
+  *x++ =  0*stride + 1, *x++ =  0*stride + 2, *x++ =  0*stride + 3,
+  *x++ =  0*stride + 1, *x++ =  1*stride + 2, *x++ =  1*stride + 3,
+  *x++ =  1*stride + 1, *x++ =  2*stride + 2, *x++ =  3*stride + 3,
+  *x++ =  1*stride + 0, *x++ =  2*stride + 1, *x++ =  3*stride + 1,
+  *x++ =  1*stride + 0, *x++ =  2*stride + 0, *x++ =  3*stride + 0,
+  *x++ =  1*stride + 0, *x++ =  2*stride - 1, *x++ =  3*stride - 1;
+/* This table approximates x^0.16 with the index being log2(x). It is clamped
+   to [-.5, 3]. The table is computed as:
+   round(256*min(3, max(.5, 1.08*(sqrt(2)*2.^([0:17]+8)/256/256).^.16))) */
+  *x++ = 128, *x++ = 134, *x++ = 150, *x++ = 168,
+  *x++ = 188, *x++ = 210, *x++ = 234, *x++ = 262,
+  *x++ = 292, *x++ = 327, *x++ = 365, *x++ = 408,
+  *x++ = 455, *x++ = 509, *x++ = 569, *x++ = 635,
+  *x++ = 710, *x++ = 768;
+/* Instead of dividing by n between 2 and 8, we multiply by 3*5*7*8/n.
+     The output is then 840 times larger, but we don't care for finding
+     the max. */
+  for (*x++ = 0, i = 1; i <= 8; i++) *x++ = 840 / i;
 };
 
-/* Generated from gen_filter_tables.c. */
-const int OD_DIRECTION_OFFSETS_TABLE[8][3] = {
-  {-1*OD_FILT_BSTRIDE + 1, -2*OD_FILT_BSTRIDE + 2, -3*OD_FILT_BSTRIDE + 3  },
-  { 0*OD_FILT_BSTRIDE + 1, -1*OD_FILT_BSTRIDE + 2, -1*OD_FILT_BSTRIDE + 3  },
-  { 0*OD_FILT_BSTRIDE + 1,  0*OD_FILT_BSTRIDE + 2,  0*OD_FILT_BSTRIDE + 3  },
-  { 0*OD_FILT_BSTRIDE + 1,  1*OD_FILT_BSTRIDE + 2,  1*OD_FILT_BSTRIDE + 3  },
-  { 1*OD_FILT_BSTRIDE + 1,  2*OD_FILT_BSTRIDE + 2,  3*OD_FILT_BSTRIDE + 3  },
-  { 1*OD_FILT_BSTRIDE + 0,  2*OD_FILT_BSTRIDE + 1,  3*OD_FILT_BSTRIDE + 1  },
-  { 1*OD_FILT_BSTRIDE + 0,  2*OD_FILT_BSTRIDE + 0,  3*OD_FILT_BSTRIDE + 0  },
-  { 1*OD_FILT_BSTRIDE + 0,  2*OD_FILT_BSTRIDE - 1,  3*OD_FILT_BSTRIDE - 1  },
-};
-
-const double OD_DERING_GAIN_TABLE[OD_DERING_LEVELS] = {
-  0, 0.5, 0.707, 1, 1.41, 2
-};
 
 /* Detect direction. 0 means 45-degree up-right, 2 is horizontal, and so on.
    The search minimizes the weighted variance along all the lines in a
@@ -58,22 +89,25 @@ const double OD_DERING_GAIN_TABLE[OD_DERING_LEVELS] = {
    in a particular direction. Since each direction have the same sum(x^2) term,
    that term is never computed. See Section 2, step 2, of:
    http://jmvalin.ca/notes/intra_paint.pdf */
-static int od_dir_find8(const od_dering_in *img, int stride, int32_t *var,
-    int coeff_shift) {
+EMSCRIPTEN_KEEPALIVE
+__attribute__((noinline))
+int od_dir_find8(const od_dering_in *img, int stride, int *tables) {
   int i;
-  int32_t cost[8] = {0};
-  int partial[8][15] = {{0}};
-  int32_t best_cost = 0;
+  int best_cost = 0;
   int best_dir = 0;
-  /* Instead of dividing by n between 2 and 8, we multiply by 3*5*7*8/n.
-     The output is then 840 times larger, but we don't care for finding
-     the max. */
-  static const int div_table[] = {0, 840, 420, 280, 210, 168, 140, 120, 105};
+  int var;
+  const int (*OD_DIRECTION_OFFSETS_TABLE)[3] = (int(*)[3])tables;
+  const int *OD_THRESH_TABLE_Q8 = tables + 8 * 3;
+  const int *div_table = OD_THRESH_TABLE_Q8 + 18;
+  int *cost = tables + (8 * 3) + 18 + 10;
+  int (*partial)[15] = (int(*)[15])(cost + 8);
+  volatile int *_rw = cost;
+  for (i = 0; i < 8 * 16; i++) _rw[i] = 0;
   for (i = 0; i < 8; i++) {
     int j;
     for (j = 0; j < 8; j++) {
       int x;
-      x = img[i*stride + j] >> coeff_shift;
+      x = img[i*stride + j] >> OD_COEFF_SHIFT;
       partial[0][i + j] += x;
       partial[1][i + j/2] += x;
       partial[2][i] += x;
@@ -117,11 +151,13 @@ static int od_dir_find8(const od_dering_in *img, int stride, int32_t *var,
   }
   /* Difference between the optimal variance and the variance along the
      orthogonal direction. Again, the sum(x^2) terms cancel out. */
-  *var = best_cost - cost[(best_dir + 4) & 7];
+  var = best_cost - cost[(best_dir + 4) & 7];
   /* We'd normally divide by 840, but dividing by 1024 is close enough
      for what we're going to do with this. */
-  *var >>= 10;
-  return best_dir;
+  var >>= 10;
+  /* We use the variance of 8x8 blocks to adjust the threshold. */
+  var = OD_MINI(32767, var >> 6);
+  return (OD_THRESH_TABLE_Q8[OD_ILOG(var)] << 3) | best_dir;
 }
 
 #define OD_DERING_VERY_LARGE (30000)
@@ -129,22 +165,26 @@ static int od_dir_find8(const od_dering_in *img, int stride, int32_t *var,
  (OD_BSIZE_MAX + 2*OD_FILT_BORDER))
 
 /* Smooth in the direction detected. */
-void od_filter_dering_direction_c(int16_t *y, int ystride, const int16_t *in,
- int ln, int threshold, int dir) {
+EMSCRIPTEN_KEEPALIVE
+__attribute__((noinline))
+void od_filter_dering_direction_8x8_c(int *y, int ystride,
+ const int *in, int threshold, int dir, const int *tables) {
   int i;
   int j;
   int k;
+  int ln = 3;
   static const int taps[3] = {3, 2, 1};
+  const int (*OD_DIRECTION_OFFSETS_TABLE)[3] = (const int(*)[3])tables;
   for (i = 0; i < 1 << ln; i++) {
     for (j = 0; j < 1 << ln; j++) {
-      int16_t sum;
-      int16_t xx;
-      int16_t yy;
+      int sum;
+      int xx;
+      int yy;
       xx = in[i*OD_FILT_BSTRIDE + j];
       sum= 0;
       for (k = 0; k < 3; k++) {
-        int16_t p0;
-        int16_t p1;
+        int p0;
+        int p1;
         p0 = in[i*OD_FILT_BSTRIDE + j + OD_DIRECTION_OFFSETS_TABLE[dir][k]]
          - xx;
         p1 = in[i*OD_FILT_BSTRIDE + j - OD_DIRECTION_OFFSETS_TABLE[dir][k]]
@@ -158,30 +198,25 @@ void od_filter_dering_direction_c(int16_t *y, int ystride, const int16_t *in,
   }
 }
 
-void od_filter_dering_direction_4x4_c(int16_t *y, int ystride,
- const int16_t *in, int threshold, int dir) {
-  od_filter_dering_direction_c(y, ystride, in, 2, threshold, dir);
-}
-
-void od_filter_dering_direction_8x8_c(int16_t *y, int ystride,
- const int16_t *in, int threshold, int dir) {
-  od_filter_dering_direction_c(y, ystride, in, 3, threshold, dir);
-}
-
 /* Smooth in the direction orthogonal to what was detected. */
-void od_filter_dering_orthogonal_c(int16_t *y, int ystride, const int16_t *in,
- const od_dering_in *x, int xstride, int ln, int threshold, int dir) {
+EMSCRIPTEN_KEEPALIVE
+__attribute__((noinline))
+void od_filter_dering_orthogonal_8x8_c(int *y, int ystride,
+ const int *in, const od_dering_in *x, int xstride, int threshold,
+ int dir, const int *tables) {
   int i;
   int j;
   int offset;
+  int ln = 3;
+  const int (*OD_DIRECTION_OFFSETS_TABLE)[3] = (const int(*)[3])tables;
   if (dir > 0 && dir < 4) offset = OD_FILT_BSTRIDE;
   else offset = 1;
   for (i = 0; i < 1 << ln; i++) {
     for (j = 0; j < 1 << ln; j++) {
-      int16_t athresh;
-      int16_t yy;
-      int16_t sum;
-      int16_t p;
+      int athresh;
+      int yy;
+      int sum;
+      int p;
       /* Deringing orthogonal to the direction uses a tighter threshold
          because we want to be conservative. We've presumably already
          achieved some deringing, so the amount of change is expected
@@ -207,144 +242,35 @@ void od_filter_dering_orthogonal_c(int16_t *y, int ystride, const int16_t *in,
   }
 }
 
-void od_filter_dering_orthogonal_4x4_c(int16_t *y, int ystride,
- const int16_t *in, const od_dering_in *x, int xstride, int threshold,
- int dir) {
-  od_filter_dering_orthogonal_c(y, ystride, in, x, xstride, 2, threshold, dir);
-}
-
-void od_filter_dering_orthogonal_8x8_c(int16_t *y, int ystride,
- const int16_t *in, const od_dering_in *x, int xstride, int threshold,
- int dir) {
-  od_filter_dering_orthogonal_c(y, ystride, in, x, xstride, 3, threshold, dir);
-}
-
-/* This table approximates x^0.16 with the index being log2(x). It is clamped
-   to [-.5, 3]. The table is computed as:
-   round(256*min(3, max(.5, 1.08*(sqrt(2)*2.^([0:17]+8)/256/256).^.16))) */
-static const int16_t OD_THRESH_TABLE_Q8[18] = {
-  128, 134, 150, 168, 188, 210, 234, 262,
-  292, 327, 365, 408, 455, 509, 569, 635,
-  710, 768,
-};
-
-/* Compute deringing filter threshold for each 8x8 block based on the
-   directional variance difference. A high variance difference means that we
-   have a highly directional pattern (e.g. a high contrast edge), so we can
-   apply more deringing. A low variance means that we either have a low
-   contrast edge, or a non-directional texture, so we want to be careful not
-   to blur. */
-static void od_compute_thresh(int thresh[OD_DERING_NBLOCKS][OD_DERING_NBLOCKS],
- int threshold, int32_t var[OD_DERING_NBLOCKS][OD_DERING_NBLOCKS],
- int nhb, int nvb) {
-  int bx;
-  int by;
-  for (by = 0; by < nvb; by++) {
-    for (bx = 0; bx < nhb; bx++) {
-      int v1;
-      /* We use the variance of 8x8 blocks to adjust the threshold. */
-      v1 = OD_MINI(32767, var[by][bx] >> 6);
-      thresh[by][bx] = (threshold*OD_THRESH_TABLE_Q8[OD_ILOG(v1)] + 128) >> 8;
-    }
-  }
-}
-
-void od_dering(const od_dering_opt_vtbl *vtbl, int16_t *y, int ystride,
- const od_dering_in *x, int xstride, int nhb, int nvb, int sbx, int sby,
- int nhsb, int nvsb, int xdec, int dir[OD_DERING_NBLOCKS][OD_DERING_NBLOCKS],
- int pli, unsigned char *bskip, int skip_stride, int threshold, int overlap,
- int coeff_shift) {
+EMSCRIPTEN_KEEPALIVE
+__attribute__((noinline))
+void od_dering(int *y, int ystride,
+ const od_dering_in *x, int xstride,
+ int pli, int threshold, int *tables) {
   int i;
   int j;
-  int bx;
-  int by;
-  int16_t inbuf[OD_DERING_INBUF_SIZE];
-  int16_t *in;
-  int bsize;
-  int32_t var[OD_DERING_NBLOCKS][OD_DERING_NBLOCKS];
-  int thresh[OD_DERING_NBLOCKS][OD_DERING_NBLOCKS];
-  bsize = 3 - xdec;
-  in = inbuf + OD_FILT_BORDER*OD_FILT_BSTRIDE + OD_FILT_BORDER;
-  /* We avoid filtering the pixels for which some of the pixels to average
-     are outside the frame. We could change the filter instead, but it would
-     add special cases for any future vectorization. */
-  for (i = 0; i < OD_DERING_INBUF_SIZE; i++) inbuf[i] = OD_DERING_VERY_LARGE;
-  for (i = -OD_FILT_BORDER*(sby != 0); i < (nvb << bsize)
-   + OD_FILT_BORDER*(sby != nvsb - 1); i++) {
-    for (j = -OD_FILT_BORDER*(sbx != 0); j < (nhb << bsize)
-     + OD_FILT_BORDER*(sbx != nhsb - 1); j++) {
-      in[i*OD_FILT_BSTRIDE + j] = x[i*xstride + j];
+  int thresh;
+  int dir = 0;
+  int *in = tables + (8 * 3) + 18 + 10 + 8 + (8* 15);
+  in += 3*OD_FILT_BSTRIDE+3;
+  for (i = -3; i < 8 + 3; i++) {
+    for (j = -3; j < 8 + 3; j++) {
+      in[i*OD_FILT_BSTRIDE + j] = x[i*ystride + j];
     }
   }
   if (pli == 0) {
-    for (by = 0; by < nvb; by++) {
-      for (bx = 0; bx < nhb; bx++) {
-        dir[by][bx] = od_dir_find8(&x[8*by*xstride + 8*bx], xstride,
-         &var[by][bx], coeff_shift);
-      }
-    }
-    od_compute_thresh(thresh, threshold, var, nhb, nvb);
+    dir = od_dir_find8(x, xstride, tables);
+    thresh = (threshold * (dir >> 3) + 128) >> 8;
+    dir &= 7;
   }
   else {
-    for (by = 0; by < nvb; by++) {
-      for (bx = 0; bx < nhb; bx++) {
-        thresh[by][bx] = threshold;
-      }
-    }
+    thresh = threshold;
   }
-  for (by = 0; by < nvb; by++) {
-    for (bx = 0; bx < nhb; bx++) {
-      int skip;
-# if defined(DAALA_ODINTRIN)
-      int xstart;
-      int ystart;
-      int xend;
-      int yend;
-      xstart = ystart = 0;
-      xend = yend = (2 >> xdec);
-      if (overlap) {
-        xstart -= (sbx != 0);
-        ystart -= (sby != 0);
-        xend += (sbx != nhsb - 1);
-        yend += (sby != nvsb - 1);
-      }
-      skip = 1;
-      /* We look at whether the current block and its 4x4 surrounding (due to
-         lapping) are skipped to avoid filtering the same content multiple
-         times. */
-      for (i = ystart; i < yend; i++) {
-        for (j = xstart; j < xend; j++) {
-          skip = skip && bskip[((by << 1 >> xdec) + i)*skip_stride
-           + (bx << 1 >> xdec) + j];
-        }
-      }
-#else
-      (void)overlap;
-      skip = bskip[by*skip_stride + bx];
-#endif
-      if (skip) thresh[by][bx] = 0;
-    }
-  }
-  for (by = 0; by < nvb; by++) {
-    for (bx = 0; bx < nhb; bx++) {
-      (vtbl->filter_dering_direction[bsize - OD_LOG_BSIZE0])(
-       &y[(by*ystride << bsize) + (bx << bsize)], ystride,
-       &in[(by*OD_FILT_BSTRIDE << bsize) + (bx << bsize)],
-       thresh[by][bx], dir[by][bx]);
-    }
-  }
-  for (i = 0; i < nvb << bsize; i++) {
-    for (j = 0; j < nhb << bsize; j++) {
+  od_filter_dering_direction_8x8_c(y, ystride, in, thresh, dir, tables);
+  for (i = 0; i < 8; i++) {
+    for (j = 0; j < 8; j++) {
       in[i*OD_FILT_BSTRIDE + j] = y[i*ystride + j];
     }
   }
-  for (by = 0; by < nvb; by++) {
-    for (bx = 0; bx < nhb; bx++) {
-      (vtbl->filter_dering_orthogonal[bsize - OD_LOG_BSIZE0])(
-       &y[(by*ystride << bsize) + (bx << bsize)], ystride,
-       &in[(by*OD_FILT_BSTRIDE << bsize) + (bx << bsize)],
-       &x[(by*xstride << bsize) + (bx << bsize)], xstride,
-       thresh[by][bx], dir[by][bx]);
-    }
-  }
+  od_filter_dering_orthogonal_8x8_c(y, ystride, in, x, xstride, thresh, dir, tables);
 }
