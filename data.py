322dce6989ccc91493dbe5f2f6ccce4376152ffd
David Michael Barr <b@rr-dav.id.au>
2016-05-04 14:05:54 +0900

    Display loglog-linear fit and dump coefficients
    Here are the results for first 144 images in subset3-mono:
    0 1.000 [ -0.80229289  32.41003226]
    0 1.083 [ -0.80131803  32.47638616]
    0 1.167 [ -0.79601721  32.4629161 ]
    0 1.250 [ -0.8042659   32.57367491]
    0 1.333 [ -0.82217186  32.7474255 ]
    0 1.417 [ -0.80753825  32.64835455]
    0 1.500 [ -0.80054852  32.64120703]
    1 1.000 [ -0.58009791  28.08700628]
    1 1.083 [ -0.67765136  29.11702044]
    1 1.167 [ -0.71297434  29.54926477]
    1 1.250 [ -0.72870588  29.73096999]
    1 1.333 [ -0.78147101  30.33638939]
    1 1.417 [ -0.81072761  30.71750524]
    1 1.500 [ -0.84403638  31.13840318]
    2 1.000 [ -0.47313939  26.98439709]
    2 1.083 [ -0.58293247  28.10790782]
    2 1.167 [ -0.6467053   28.79500311]
    2 1.250 [ -0.66207353  28.9810467 ]
    2 1.333 [ -0.728696    29.70832727]
    2 1.417 [ -0.77622865  30.25673405]
    2 1.500 [ -0.82395756  30.81040799]
    3 1.000 [ -0.42801831  26.36537597]
    3 1.083 [ -0.63942557  28.40627501]
    3 1.167 [ -0.85400404  30.4945478 ]
    3 1.250 [ -1.07614288  32.67670318]
    3 1.333 [ -1.27412254  34.75438502]
    3 1.417 [ -1.33278889  35.4257851 ]
    3 1.500 [ -1.42764519  36.46161139]
    
    Signed-off-by: David Michael Barr <b@rr-dav.id.au>

--- data.py
+++ data.py
@@ -1,15 +1,22 @@
 stats = [[],[],[],[]] # Get from job output
 stats = [[band[off::16] for off in range(7)] for band in stats]
 from matplotlib import colors, pyplot
+import numpy as np
 f, ((ax1, ax2), (ax3, ax4)) = pyplot.subplots(2, 2, sharex=True, sharey=True)
-for off in range(7):
-  ax1.loglog(range(421,815), stats[0][off][421:815], '-', label='%0.3f'%(1.+off/12.))
-for off in range(7):
-  ax2.loglog(range(421,815), stats[1][off][421:815], '-', label='%0.3f'%(1.+off/12.))
-for off in range(7):
-  ax3.loglog(range(421,815), stats[2][off][421:815], '-', label='%0.3f'%(1.+off/12.))
-for off in range(7):
-  ax4.loglog(range(421,815), stats[3][off][421:815], '-', label='%0.3f'%(1.+off/12.))
-ax1.set_xlim([421, 815])
+ax = [ax1, ax2, ax3, ax4]
+xlog = np.log2(range(421,815))
+def loglogfit(off, band):
+  ylog = np.log2(np.asarray(stats[band][off][421:815], dtype=float))
+  coeff = np.polyfit(xlog, ylog, 1)
+  fit = np.poly1d(coeff)
+  y = np.exp2(fit(xlog))
+  label = '%0.3f'%(1.+off/12.)
+  ax[band].plot(range(421,815), y, '-', label=label)
+  print(band, label, coeff)
+for band in range(4):
+  for off in range(7):
+    loglogfit(off, band)
+ax[0].set_xlim([421, 815])
 pyplot.legend()
 pyplot.show()
+
