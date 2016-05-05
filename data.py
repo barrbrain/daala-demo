e7e7b0a613dfac81da87136806b2c9e748bd8b2e
David Michael Barr <b@rr-dav.id.au>
2016-05-05 09:48:30 +0900

    Read band stats json from file
    9 plots to cover all block sizes.
    Display optimal QM value for each band.
    
    Signed-off-by: David Michael Barr <b@rr-dav.id.au>

--- data.py
+++ data.py
@@ -1,22 +1,35 @@
-stats = [[],[],[],[]] # Get from job output
-stats = [[band[off::16] for off in range(7)] for band in stats]
+import json
 from matplotlib import colors, pyplot
 import numpy as np
-f, ((ax1, ax2), (ax3, ax4)) = pyplot.subplots(2, 2, sharex=True, sharey=True)
-ax = [ax1, ax2, ax3, ax4]
-xlog = np.log2(range(421,815))
+import sys
+stats = json.loads(open(sys.argv[1], 'r').read())
+stats = [[band[off::16] for off in range(12,19)] for band in stats]
+f, ((ax1, ax2, ax3), (ax4, ax5, ax6), (ax7, ax8, ax9)) = pyplot.subplots(3, 3, sharex=True, sharey=True)
+ax = [ax1, ax2, ax3, ax4, ax5, ax6, ax7, ax8, ax9]
+coeffs = {}
 def loglogfit(off, band):
-  ylog = np.log2(np.asarray(stats[band][off][421:815], dtype=float))
+  y = np.asarray(stats[band][off], dtype=float)
+  x = y.nonzero()[0]
+  if (x.size == 0): return
+  xlog = np.log2(x)
+  ylog = np.log2(y[x])
   coeff = np.polyfit(xlog, ylog, 1)
   fit = np.poly1d(coeff)
   y = np.exp2(fit(xlog))
   label = '%0.3f'%(1.+off/12.)
-  ax[band].plot(range(421,815), y, '-', label=label)
-  print(band, label, coeff)
-for band in range(4):
+  ax[band].loglog(x, y, '-', label=label)
+  ax[band].grid(True, which='both')
+  coeffs[(band, label)] = coeff
+  print(band, label, coeff, x.size)
+for band in range(len(stats)):
   for off in range(7):
     loglogfit(off, band)
-ax[0].set_xlim([421, 815])
+  b1 = coeffs[(band, '1.000')]
+  b1_5 = coeffs[(band, '1.500')]
+  qm_421 = np.exp2((np.log2(421)*b1[0]+b1[1]-b1_5[1])/b1_5[0])*16/421
+  qm_814 = np.exp2((np.log2(814)*b1[0]+b1[1]-b1_5[1])/b1_5[0])*16/814
+  print(np.sqrt(qm_421*qm_814))
+ax[0].set_xlim([421, 814])
 pyplot.legend()
 pyplot.show()
 
