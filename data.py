b608c06e4e6b486fd0e6af4bd906d626fea8f212
David Michael Barr <b@rr-dav.id.au>
2016-05-08 02:07:41 +0900

    Display inferred QM values in fully covered range
    Signed-off-by: David Michael Barr <b@rr-dav.id.au>

--- data.py
+++ data.py
@@ -2,8 +2,10 @@ import json
 from matplotlib import colors, pyplot
 import numpy as np
 import sys
+from scipy.interpolate import interp1d
+from scipy.interpolate import UnivariateSpline
 stats = json.loads(open(sys.argv[1], 'r').read())
-stats = [[band[off::16] for off in range(12,19)] for band in stats]
+stats = [[band[off::16] for off in range(7)] for band in stats]
 f, ((ax1, ax2, ax3), (ax4, ax5, ax6), (ax7, ax8, ax9)) = pyplot.subplots(3, 3, sharex=True, sharey=True)
 ax = [ax1, ax2, ax3, ax4, ax5, ax6, ax7, ax8, ax9]
 coeffs = {}
@@ -13,23 +15,23 @@ def loglogfit(off, band):
   if (x.size == 0): return
   xlog = np.log2(x)
   ylog = np.log2(y[x])
-  coeff = np.polyfit(xlog, ylog, 1)
-  fit = np.poly1d(coeff)
-  y = np.exp2(fit(xlog))
   label = '%0.3f'%(1.+off/12.)
-  ax[band].loglog(x, y, '-', label=label)
+  inverse = interp1d(ylog, xlog)
+  # coeffs[(band, label+'i')] = inverse
+  x = np.arange(24,492)
+  b1 = np.log2(np.asarray(stats[band][0], dtype=float)[x])
+  qm = inverse(b1)-np.log2(x)
+  ax[band].plot(x, np.exp2(qm)*16, '-', label=label)
   ax[band].grid(True, which='both')
-  coeffs[(band, label)] = coeff
-  print(band, label, coeff, x.size)
+  print(band, band - band//3, label, np.exp2(qm.mean())*16)
 for band in range(len(stats)):
-  for off in range(7):
+  for off in range(1,7):
     loglogfit(off, band)
-  b1 = coeffs[(band, '1.000')]
-  b1_5 = coeffs[(band, '1.500')]
-  qm_421 = np.exp2((np.log2(421)*b1[0]+b1[1]-b1_5[1])/b1_5[0])*16/421
-  qm_814 = np.exp2((np.log2(814)*b1[0]+b1[1]-b1_5[1])/b1_5[0])*16/814
-  print(np.sqrt(qm_421*qm_814))
-ax[0].set_xlim([421, 814])
+  #bitrate = min(np.log2(stats[band][i][9]) for i in range(7))
+  #print(max(np.exp2(coeffs[(band, '%0.3fi'%(1.+i/12.))](bitrate)) for i in range(7)))
+  #bitrate = max(np.log2(stats[band][i][1015]) for i in range(7))
+  #print(min(np.exp2(coeffs[(band, '%0.3fi'%(1.+i/12.))](bitrate)) for i in range(7)))
+ax[0].set_xlim([24, 492])
 pyplot.legend()
 pyplot.show()
 
